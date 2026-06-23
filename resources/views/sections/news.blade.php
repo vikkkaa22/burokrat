cfa51c89ce94227427bb85bb7f89960cf8725cfa
Unity <Unity@rbiu.local>
2026-06-23 11:53:10 +0500

    cards

---
+++ resources/views/sections/news.blade.php
@@ -0,0 +1,45 @@
+<section class="events">
+    <div class="container">
+        <h1 class="title1">СОБЫТИЯ И НОВОСТИ</h1>
+
+        <div class="events_nav">
+            <button class="btn-nav">&#8592;</button>
+            <button class="btn-nav">&#8594;</button>
+        </div>
+        <div class="events_grid">
+            <article class="event-card">
+                <img src="img/img1.png" alt="">
+                <div class="card_content">
+                    <h3>Фестиваль «Между домами»</h3>
+                    <p>21 июня с 13:00–20:00</p>
+                </div>
+            </article>
+
+            <article class="event-card">
+                <img src="img/img2.png" alt="">
+                <div class="card_content">
+                    <h3>Ретрозабег по Старгороду</h3>
+                    <p>14 июня, 5 июля, 15 августа в 10:00</p>
+                </div>
+            </article>
+            <article class="event-card">
+                <img src="img/img3.png" alt="">
+                <div class="card_content">
+                    <h3>Регистрация на забег</h3>
+                    <p>14 июня</p>
+                </div>
+            </article>
+            <article class="event-card">
+                <img src="img/img4.png" alt="">
+                <div class="card_content">
+                    <h3>Фестиваль «Между домами»</h3>
+                    <p>21 июня с 13:00–20:00</p>
+                </div>
+            </article>
+        </div>
+        <div class="events_footer">
+            <a href="#" class="btn_brn">Забронировать</a>
+
+        </div>
+    </div>
+</section>
