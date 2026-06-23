c4e4bbeeb2572513d743047973a211528354a633
vikkkaa22 <kusya2203@gmail.com>
2026-06-23 11:53:53 +0500

    made header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,28 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-main">
-    <header>
-        <h1>хедер</h1>
+<body class="bg-bg">
+    <header class="bg-bg sticky">
+        <div class="mt-10 flex justify-between items-center max-w-6xl mx-auto">
+            <div class="logo">
+                <a href="/">
+                    <img src="../../public/img/logo.svg" alt="Логотип">
+                </a>
+            </div>
+            <nav>
+                <ul class="text-gray-xl font-main flex space-x-6">
+                    <li><a href="sections/about-city.blade.php">О городе</a></li>
+                    <li><a href="sections/about-place.blade.php">Бюрократъ</a></li>
+                    <li><a href="sections/museum.blade.php">Музей</a></li>
+                    <li><a href="sections/catalog.blade.php">Сувенирная лавка</a></li>
+                    <li><a href="sections/coffee.blade.php">Кофе-зона</a></li>
+                    <li><a href="sections/book.blade.php">Кроссбукинг</a></li>
+                    <li><a href="sections/show.blade.php">Шоу</a></li>
+                    <li><a href="sections/news.blade.php">Афиша</a></li>
+                    <li><a href="sections/contacts.blade.php">Контакты</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
     <main>
          @include('sections.about-city')
@@ -33,8 +52,11 @@
     <img src="img/1.jpg" alt="tiger">
     <p class="bg-bg text-red">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repudiandae possimus dolorum fugit illo rerum illum expedita minus rem quis voluptate iure quam cum, facere blanditiis fuga accusantium odit natus?</p>   
     </main>
-    <footer>
-        подвал
+    <footer class="bg-gray text-white font-main flex h-596">
+        <ul >
+            <li>+7 999 999 99 99</li>
+            <li>burokrat_museum@mail.ru</li>
+        </ul>
     </footer>
     
 </body>
