08f42b7d00018f413508949b400e750e8ef49b5c
oshiba777 <stik_xi@mail.ru>
2026-06-23 12:56:08 +0500

    верстка штор

--- resources/views/sections/show.blade.php
+++ resources/views/sections/show.blade.php
@@ -12,15 +12,16 @@
     <div class="grid-row">
         <img class=" mb-[135px] w-full" src="img/coving.png" alt="падуга">
         
-    <div class="curtains">
-        <img class=" " src="img/l_curtains.png" alt="левая штора">
-        <img src="img/r_curtains.png" alt="правая штора">
-    </div>
-
-    <div class="content" >
+    <div class="content text-center" >
         <h2 class="text-[64px] text-red font-title font-bold mb-[25px] ">НАШЕ ШОУ</h2>
         <p class="text-[20px] text-gray font-main mb-[55px]">Информация о шоу скоро появится.<br>Следите за анонсами в наших соцсетях и на этом месте.</p>
         <button class="text-[20px] text-white font-main bg-red rounded-[50px] px-[69px] py-[10px] max-w-[350px] ">Перейти в ВК-видео</button>
+    </div>
+
+    <div class="relative h-[820px] overflow-hidden">
+        <h2 class="text-[64px] text-white font-title font-bold mb-[25px] text-center">НАШЕ ШОУ</h2>
+        <img class="absolute top-0 left-0 h-full w-1/2 object-cover" src="img/l_curtains.png" alt="левая штора">
+        <img class="absolute top-0 right-0 h-full w-1/2 object-cover" src="img/r_curtains.png" alt="правая штора">
     </div>
 </div>
    
