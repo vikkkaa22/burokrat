030ef1339ba0387172b4845632bb9572d51079d5
Unity <Unity@rbiu.local>
2026-06-23 11:53:32 +0500

    верстка контента шоу

---
+++ resources/views/sections/show.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>БюрократЪ</title>
+
+</head>
+
+<body class="flex-col">
+    <div class="grid-row">
+        <img class=" mb-[135px]" src="img/coving.png" alt="падуга">
+        
+    <div class="curtains">
+        <img class=" " src="img/l_curtains.png" alt="левая штора">
+        <img src="img/r_curtains.png" alt="правая штора">
+    </div>
+
+    <div class="content" >
+        <h2 class="text-[64px] text-red font-title font-bold mb-[25px] ">НАШЕ ШОУ</h2>
+        <p class="text-[20px] text-gray font-main mb-[55px]">Информация о шоу скоро появится.<br>Следите за анонсами в наших соцсетях и на этом месте.</p>
+        <button class="text-[20px] text-white font-main bg-red rounded-[50px] px-[69px] py-[10px] max-w-[350px] ">Перейти в ВК-видео</button>
+    </div>
+</div>
+   
+     
+
+</body>
+
+</html>
