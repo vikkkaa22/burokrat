0cfaf127334b1ba7993be92beedbc909d1ca0981
Unity <Unity@rbiu.local>
2026-06-25 11:08:46 +0500

    about_iter1

---
+++ resources/views/sections/about-place.blade.php
@@ -0,0 +1,33 @@
+<div class="bg-red w-full p-[120px]">
+    <h1 class="font-title text-[64px] uppercase text-white mb-[70px]">
+        О ПРОСТРАНСТВЕ «БЮРОКРАТЪ»
+    </h1>
+    <div class="flex gap-x-[20px] mb-[70px]">
+        <div class="flex-1">
+            <p class="font-main text-[20px] leading-[120%] mb-[32px] text-white">
+                Здесь история встречается с современной городской жизнью. Здесь
+                можно рассматривать экспонаты, выпить кофе, полистать книги,
+                пообщаться с друзьями и найти необычные сувениры.
+            </p>
+            <h3
+                class="font-title text-[44px] uppercase leading-[120%] text-white"
+            >
+                Мы — живое, интерактивное пространство в историческом центре
+                города Миасс!
+            </h3>
+        </div>
+        <img class="flex-1" src="img/img_about-1.png" alt="">
+    </div>
+    <div class="flex gap-x-[20px] mb-[70px]">
+        <img class="flex-1" src="img/img_about-2.png" alt="">
+        <img class="flex-1" src="img/img_about-3.png" alt="">
+        <img class="flex-1" src="img/img_about-4.png" alt="">
+    </div>
+    <div class="flex gap-x-[20px]">
+        <img src="img/img_about-5.png" alt="" class="flex-1">
+        <div class="flex-1">
+            <h3 class="font-title text-[44px] uppercase leading-[120%] mb-[32px] text-white">Почему «бюрократЪ?»</h3>
+            <p class="font-main text-[20px] leading-[120%] text-white">Название отсылает к атмосфере старых бюро, архивов и учреждений прошлого века. Мы сохранили этот характер в интерьере, наполнив пространство предметами, которые помогают по‑новому взглянуть на историю и провести время в особой ретро-атмосфере.</p>
+        </div>
+    </div>
+</div>
