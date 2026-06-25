463bbb41e031fec60d0bbbd294fe3f8e35393ceb
Unity <Unity@rbiu.local>
2026-06-25 11:52:34 +0500

    catalog start

--- resources/views/sections/catalog.blade.php
+++ resources/views/sections/catalog.blade.php
@@ -1,4 +1,66 @@
-<section>
-    <h2>Каталог</h2>
-    карточки
+<section class="container mx-auto px-[120px]">
+<div class="relative mb-8 lg:mb-10">
+                    <div class="absolute top-0 left-0 text-[70px] md:text-[100px] xl:text-[128px] font-bold uppercase text-red font-main blur-[6px] opacity-30 pointer-events-none leading-none">
+                        СУВЕНИРНАЯ ЛАВКА
+                    </div>
+                    <h2 class="relative z-10 text-[36px] md:text-[48px] xl:text-[64px] uppercase font-title font-bold text-red">
+                        СУВЕНИРНАЯ ЛАВКА
+                    </h2>
+                </div>
+
+    <div class="grid grid-cols-2 gap-5 p-5 container mx-auto ">
+        <div class="card relative  h-[393px]  overflow-hidden">
+            <img src="/img/catalog1.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red  to-transparent"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                        <span class="text-white font-main xl font-medium">Сумка-шоппер ВМиасс</span>
+                        <span class="text-white font-main xl font-medium">890р.</span>
+            </div>
+        </div>
+        
+         <div class="card relative  h-[393px] overflow-hidden">
+            <img src="/img/catalog2.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                        <span class="text-white font-main xl font-medium">Ручка шариковая синий бамбук ВМиасс</span>
+                        <span class="text-white font-main xl font-medium">230р.</span>
+            </div>
+        </div>
+
+        <div class="card relative  h-[393px] overflow-hidden">
+            <img src="/img/catalog3.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                        <span class="text-white font-main xl font-medium">Кружка из пшеничного волокна ВМиасс 320мл</span>
+                        <span class="text-white font-main xl font-medium">399р.</span>
+            </div>
+        </div>
+
+        <div class="card relative  h-[393px] overflow-hidden">
+            <img src="/img/catalog4.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                        <span class="text-white font-main xl font-medium">Брелок деревянный овальный ВМиасс</span>
+                        <span class="text-white font-main xl font-medium">195р.</span>
+            </div>
+        </div>
+
+        <div class="card relative  h-[393px] overflow-hidden">
+            <img src="/img/catalog5.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                        <span class="text-white font-main xl font-medium">Магнит акриловый 8*7см ассорти ВМиасс</span>
+                        <span class="text-white font-main xl font-medium">199р.</span>
+            </div>
+        </div>
+
+        <div class="card relative  h-[393px] overflow-hidden">
+            <img src="/img/catalog6.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                        <span class="text-white font-main xl font-medium">Ручка шариковая синий автомат из пшеничного волокна ВМиасс</span>
+                        <span class="text-white font-main xl font-medium">175р.</span>
+            </div>
+
+    </div>
 </section>
