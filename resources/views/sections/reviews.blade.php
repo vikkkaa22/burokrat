2296b88ec631d16f6b5d0d536f3c10de404ba21a
oshiba777 <stik_xi@mail.ru>
2026-06-25 11:08:48 +0500

    шторки

---
+++ resources/views/sections/reviews.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <div>
+            <h2>ОТЗЫВЫ</h2>
+            <div>
+                <h3>Мы собрали не просто слова,
+                    а настоящие впечатления гостей</h3>
+                <p>В виде фотографий рукописных отзывов. Каждый лист хранит живые эмоции, мысли и атмосферу нашего пространства.</p>
+            </div>
+
+        </div>
+        <div>
+            <div class="img">
+
+            </div>
+            <div>
+                <button class="bg-red"><img src="img/Arrow 2.svg" alt=""></button>
+                <button class="bg-red"><img src="img/Arrow 1.svg" alt=""></button>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
--- resources/views/sections/show.blade.php
+++ resources/views/sections/show.blade.php
@@ -9,23 +9,27 @@
 </head>
 
 <body class="flex-col">
-    <div class="grid-row">
-        <img class=" mb-[135px] w-full" src="img/coving.png" alt="падуга">
-        
-    <div class="content text-center" >
-        <h2 class="text-[64px] text-red font-title font-bold mb-[25px] ">НАШЕ ШОУ</h2>
-        <p class="text-[20px] text-gray font-main mb-[55px]">Информация о шоу скоро появится.<br>Следите за анонсами в наших соцсетях и на этом месте.</p>
-        <button class="text-[20px] text-white font-main bg-red rounded-[50px] px-[69px] py-[10px] max-w-[350px] ">Перейти в ВК-видео</button>
+    <div class="theater grid-row relative overflow-hidden">
+        <img class=" relative z-50 mb-[135px] w-full" src="img/coving.png" alt="падуга">
+
+        <div class="content text-center mb-[280px] mt-[280px]">
+            <h2 class="text-[64px] text-red font-title font-bold mb-[25px] ">НАШЕ ШОУ</h2>
+            <p class="text-[20px] text-gray font-main mb-[55px]">Информация о шоу скоро появится.<br>Следите за анонсами
+                в наших соцсетях и на этом месте.</p>
+            <button
+                class="text-[20px] text-white font-main bg-red rounded-[50px] px-[69px] py-[10px] max-w-[350px] ">Перейти
+                в ВК-видео</button>
+        </div>
+
+        <div class=" h-[820px] w-full overflow-hidden absolute top-0 left-0 w-full h-full">
+            <h2 class="text-show absolute inset-0 z-10 flex items-center justify-center text-[64px] text-white font-title font-bold">
+                НАШЕ ШОУ</h2>
+            <img class="curtain1 absolute top-0 left-0 h-full w-1/2 object-cover" src="img/l_curtains.png" alt="левая штора">
+            <img class=" curtain2 absolute top-0 right-0 h-full w-1/2 object-cover" src="img/r_curtains.png" alt="правая штора">
+        </div>
     </div>
 
-    <div class="relative h-[820px] overflow-hidden">
-        <h2 class="text-[64px] text-white font-title font-bold mb-[25px] text-center">НАШЕ ШОУ</h2>
-        <img class="absolute top-0 left-0 h-full w-1/2 object-cover" src="img/l_curtains.png" alt="левая штора">
-        <img class="absolute top-0 right-0 h-full w-1/2 object-cover" src="img/r_curtains.png" alt="правая штора">
-    </div>
-</div>
-   
-     
+
 
 </body>
 
