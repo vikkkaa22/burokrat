291808d103a52bb92616c029034c9223b70f177c
Unity <Unity@rbiu.local>
2026-06-29 13:21:27 +0500

    cofee

---
+++ resources/views/sections/coffee.blade.php
@@ -0,0 +1,48 @@
+<section class="w-full py-24 bg-bg">
+    <div class="max-w-[1200px] mx-auto">
+
+         <!-- Заголовок -->
+        <div class="relative mb-16 ">
+            <div class="absolute -top-12 text-[200px] font-bold uppercase text-red font-main blur-[6px] opacity-30">
+                БЮРОКРАТЪ
+            </div>
+
+            <h2 class="relative z-10 text-[96px] uppercase font-title font-bold text-red">
+                Кофе-зона
+            </h2>
+        </div>
+                    <p class="mb-10 max-w-[448px] text-gray text-xl font-main font-medium mb-10">
+                Музей старины, кофейня и сувениры. Лучшее место для ностальгии,
+                книг и уникальных фото в исторической атмосфере.
+            </p>
+                    <p class="mt-2 max-w-[700px] text-gray uppercase font-main font-medium leading-[1.2] text-[40px]"> Вторая зона ретро-пространства «Бюрократъ» — это кофе-зона «История Миасса» Звучит пафосно? А вы попробуйте</p>
+                </div>
+
+                 <!-- Фото -->
+                <div class=" flex flex-col ">
+                <img src="/img/frameTop.png" alt="" class="absolute mt-[-400px] ml-270 z-20  ">
+                <div class="mt-[-360px] ml-280 w-[500px]">
+                    <img
+                        src="/img/столик.png"
+                        alt=""
+                        class="w-full h-[600px] object-cover grayscale"
+                    >
+                </div>
+                <div class=" mt-[-190px] ml-80 w-[500px]">
+                    <img
+                        src="/img/кофе.png"
+                        alt=""
+                        class="w-full h-[600px] object-cover grayscale8"
+                    >
+                </div>
+            </div>
+<img src="/img/frameBot.png" alt="" class="absolute mt-[-65px] ml-70 z-20  ">
+            <p class="mb-10 mt-[-350px] max-w-[448px] ml-230 text-gray text-xl font-main font-medium">
+               Вас ждёт ароматный чай или кофе с десертом в окружении деталей, а также очень удобные советские кресла, которые рассказывают историю Миасса громче любого экскурсовода. Вы просто должны прочувствовать все на себе!
+            </p>
+                    <p class=" max-w-[700px] ml-230 text-gray uppercase font-main font-medium leading-[1.2] text-[40px]"> Мы не просто наливаем напитки.Мы даём вам выпить чашку истории.</p>
+                </div>
+
+
+ </div>
+</section>
