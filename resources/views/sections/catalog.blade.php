35b23745b4c559b9bdd166bbab8222c42ff3ae00
Unity <Unity@rbiu.local>
2026-06-29 13:21:05 +0500

    анимация

--- resources/views/sections/catalog.blade.php
+++ resources/views/sections/catalog.blade.php
@@ -1,68 +1,73 @@
 
-<section class="container mx-auto px-[120px]">
-<div class="relative mb-8 lg:mb-10">
-                    <div class="absolute top-0 left-0 text-[70px] md:text-[100px] xl:text-[128px] font-bold uppercase text-red font-main blur-[6px] opacity-30 pointer-events-none leading-none">
-                        бюрократъ
-                    </div>
-                    <h2 class="relative z-10 text-[36px] md:text-[48px] xl:text-[64px] uppercase font-title font-bold text-red">
-                        СУВЕНИРНАЯ ЛАВКА
-                    </h2>
-                </div>
+<section class="container mx-auto px-4 md:px-[60px] lg:px-[120px]">
+    <div class="relative mb-8 lg:mb-10">
+        <div class="absolute top-0 left-0 text-[70px] md:text-[100px] xl:text-[128px] font-bold uppercase text-red font-main blur-[6px] opacity-30 pointer-events-none leading-none">
+            БЮРОКРАТЬ
+        </div>
+        <h2 class="relative z-10 text-[36px] md:text-[48px] xl:text-[64px] uppercase font-title font-bold text-red">
+            СУВЕНИРНАЯ ЛАВКА
+        </h2>
+    </div>
 
-    <div class="grid grid-cols-2 gap-5 p-5 container mx-auto ">
-        <div class="card relative  h-[393px]  overflow-hidden">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 md:gap-5 p-2 md:p-5 container mx-auto">
+
+        <div class="card relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[393px] overflow-hidden group">
             <img src="/img/catalog1.JPG" alt="" class="w-full h-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-red  to-transparent"></div>
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
             <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
-                        <span class="text-white font-main xl font-medium">Сумка-шоппер ВМиасс</span>
-                        <span class="text-white font-main xl font-medium">890р.</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">Сумка-шоппер ВМиасс</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">890р.</span>
             </div>
         </div>
-        
-         <div class="card relative  h-[393px] overflow-hidden">
+
+
+        <div class="card relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[393px] overflow-hidden group">
             <img src="/img/catalog2.JPG" alt="" class="w-full h-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
             <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
-                        <span class="text-white font-main xl font-medium">Ручка шариковая синий бамбук ВМиасс</span>
-                        <span class="text-white font-main xl font-medium">230р.</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">Ручка шариковая синий бамбук ВМиасс</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">230р.</span>
             </div>
         </div>
 
-        <div class="card relative  h-[393px] overflow-hidden">
+
+        <div class="card relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[393px] overflow-hidden group">
             <img src="/img/catalog3.JPG" alt="" class="w-full h-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
             <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
-                        <span class="text-white font-main xl font-medium">Кружка из пшеничного волокна ВМиасс 320мл</span>
-                        <span class="text-white font-main xl font-medium">399р.</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">Кружка из пшеничного волокна ВМиасс 320мл</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">399р.</span>
             </div>
         </div>
 
-        <div class="card relative  h-[393px] overflow-hidden">
+
+        <div class="card relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[393px] overflow-hidden group">
             <img src="/img/catalog4.JPG" alt="" class="w-full h-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
             <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
-                        <span class="text-white font-main xl font-medium">Брелок деревянный овальный ВМиасс</span>
-                        <span class="text-white font-main xl font-medium">195р.</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">Брелок деревянный овальный ВМиасс</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">195р.</span>
             </div>
         </div>
 
-        <div class="card relative  h-[393px] overflow-hidden">
+ 
+        <div class="card relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[393px] overflow-hidden group">
             <img src="/img/catalog5.JPG" alt="" class="w-full h-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
             <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
-                        <span class="text-white font-main xl font-medium">Магнит акриловый 8*7см ассорти ВМиасс</span>
-                        <span class="text-white font-main xl font-medium">199р.</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">Магнит акриловый 8*7см ассорти ВМиасс</span>
+                <span class="font-main xl font-medium text-gray group-hover:text-white transition-colors duration-300">199р.</span>
             </div>
         </div>
 
-        <div class="card relative  h-[393px] overflow-hidden">
-            <img src="/img/catalog6.JPG" alt="" class="w-full h-full object-cover">
-            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent"></div>
-            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
-                        <span class="text-white font-main xl font-medium">Ручка шариковая синий автомат из пшеничного волокна ВМиасс</span>
-                        <span class="text-white font-main xl font-medium">175р.</span>
-            </div>
 
+        <div class="card relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[393px] overflow-hidden group">
+            <img src="/img/catalog6.JPG" alt="" class="w-full h-full object-cover">
+            <div class="absolute inset-0 bg-gradient-to-t from-red to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+            <div class="absolute bottom-5 left-5 right-5 flex justify-between items-center z-10">
+                <span class="font-main xl font-medium text-grey group-hover:text-white transition-colors duration-300">Ручка шариковая синий автомат из пшеничного волокна ВМиасс</span>
+                <span class="font-main xl font-medium text-grey group-hover:text-white transition-colors duration-300">175р.</span>
+            </div>
+        </div>
     </div>
 </section>
-
