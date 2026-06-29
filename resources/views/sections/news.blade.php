76a9ebc02443891687b617d6ae20e0c27359e1a2
Arina2810 <abuivan@inbox.ru>
2026-06-29 11:08:58 +0500

    cards

--- resources/views/sections/news.blade.php
+++ resources/views/sections/news.blade.php
@@ -1,14 +1,21 @@
 <section class="events">
     <div class="container">
+        <div class="relative mb-8 lg:mb-10">
+            <div
+                class="absolute top-0 left-0 text-[70px] md:text-[100px] xl:text-[128px] font-bold uppercase text-red font-main blur-[6px] opacity-30 pointer-events-none leading-none"
+            >
+                бюрократъ
+            </div>
+        </div>
         <h1 class="title1">СОБЫТИЯ И НОВОСТИ</h1>
 
         <div class="events_nav">
             <button class="btn-nav">&#8592;</button>
             <button class="btn-nav">&#8594;</button>
         </div>
-        <div class="events_grid">
+        <!-- <div class="events_grid">
             <article class="event-card">
-                <img src="img/img1.png" alt="">
+                <img src="img/img1.png" alt="" />
                 <div class="card_content">
                     <h3 class="font-medium">Фестиваль «Между домами»</h3>
                     <p>21 июня с 13:00–20:00</p>
@@ -16,45 +23,76 @@
             </article>
 
             <article class="event-card">
-                <img src="img/img2.png" alt="">
+                <img src="img/img2.png" alt="" />
                 <div class="card_content">
                     <h3 class="font-medium">Ретрозабег по Старгороду</h3>
                     <p>14 июня, 5 июля, 15 августа в 10:00</p>
                 </div>
             </article>
             <article class="event-card">
-                <img src="img/img3.png" alt="">
+                <img src="img/img3.png" alt="" />
                 <div class="card_content">
                     <h3 class="font-medium">Регистрация на забег</h3>
                     <p>14 июня</p>
                 </div>
             </article>
             <article class="event-card">
-                <img src="img/img4.png" alt="">
+                <img src="img/img4.png" alt="" />
                 <div class="card_content">
                     <h3 class="font-medium">Фестиваль «Между домами»</h3>
                     <p>21 июня с 13:00–20:00</p>
                 </div>
             </article>
-        </div>
+        </div> -->
         <div class="events_footer">
             <a href="#" class="btn_brn">Забронировать</a>
-
         </div>
     </div>
 </section>
 <section>
-<div class="embla" id="my-carousel">
-    <div class="embla__viewport">
-        <div class="embla__container">
-            <div class="embla__slide">Слайд 1</div>
-            <div class="embla__slide">Слайд 2</div>
-            <div class="embla__slide">Слайд 3</div>
+    <div class="events_grid">
+        <div class="embla" id="my-carousel">
+            <div class="embla__viewport">
+                <div class="embla__container">
+                    <div class="embla__slide">
+                        <article class="event-card">
+                            <img src="img/img1.png" alt="" />
+                            <div class="card_content">
+                                <h3 class="font-medium">
+                                    Фестиваль «Между домами»
+                                </h3>
+                                <p>21 июня с 13:00–20:00</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="embla__slide">
+                        <article class="event-card">
+                            <img src="img/img2.png" alt="" />
+                            <div class="card_content">
+                                <h3 class="font-medium">
+                                    Ретрозабег по Старгороду
+                                </h3>
+                                <p>14 июня, 5 июля, 15 августа в 10:00</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="embla__slide">
+                        <article class="event-card">
+                            <img src="img/img3.png" alt="" />
+                            <div class="card_content">
+                                <h3 class="font-medium">
+                                    Регистрация на забег
+                                </h3>
+                                <p>14 июня</p>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Кнопки управления (опционально) -->
+            <button class="embla__prev">Назад</button>
+            <button class="embla__next">Вперед</button>
         </div>
     </div>
-
-    <!-- Кнопки управления (опционально) -->
-    <button class="embla__prev">Назад</button>
-    <button class="embla__next">Вперед</button>
-</div>
 </section>
