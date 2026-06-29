d1d6f2315e5f7a01313c34c8fbf813b0f5d5884b
oshiba777 <stik_xi@mail.ru>
2026-06-29 13:22:43 +0500

    шторы js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,28 @@
 import './bootstrap';
-import 'flowbite'; 
+import 'flowbite';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    
+    if (document.querySelector('.theater')) {
+        const theater = document.querySelector('.theater');
+        const curtain1 = document.querySelector('.curtain1');
+        const curtain2 = document.querySelector('.curtain2');
+        const text_show = document.querySelector('.text_show');
+    
+
+
+        theater.addEventListener("click", function () {
+            curtain1.classList.add("animate1");
+            curtain2.classList.add("animate2");
+            text_show.classList.add("animate3");  
+        });
+    }
+    
+});
--- resources/views/sections/show.blade.php
+++ resources/views/sections/show.blade.php
@@ -10,22 +10,30 @@
 
 <body class="flex-col">
     <div class="theater grid-row relative overflow-hidden">
-        <img class=" relative z-50  w-full" src="img/coving.png" alt="падуга">
+        <img class=" relative z-10  w-full" src="img/coving.png" alt="падуга">
 
-        <div class="content text-center mb-[260px] mt-[260px]">
+        <div class="content text-center mb-[200px] mt-[200px]">
             <h2 class="text-[64px] text-red font-title font-bold mb-[25px] ">НАШЕ ШОУ</h2>
             <p class="text-[20px] text-gray font-main mb-[50px]">Информация о шоу скоро появится.<br>Следите за анонсами
                 в наших соцсетях и на этом месте.</p>
-            <button
-                class="text-[20px] text-white font-main bg-red rounded-[50px] px-[69px] py-[10px] max-w-[350px] hover:bg-red_hover transition">Перейти
-                в ВК-видео</button>
+                <a href="https://vk.ru/burokrat_museum" class="relative text-[20px] text-white font-main bg-red rounded-[50px] px-[69px] py-[10px] max-w-[350px] hover:bg-red_hover transition">Перейти
+                в ВК-видео</a>
         </div>
 
-        <div class=" h-[820px] w-full overflow-hidden absolute top-0 left-0 w-full h-full ">
-            <h2 class="text-show absolute inset-0 z-10 flex items-center justify-center text-[64px] text-white font-title font-bold">
+        <div class=" h-[820px] w-full overflow-hidden absolute top-0 left-0 w-full h-full " style="pointer-events: none;">
+            <h2
+                class="text_show absolute inset-0 z-10 flex items-center justify-center text-[64px] text-white font-title font-bold">
                 НАШЕ ШОУ</h2>
-            <img class="curtain1 absolute top-0 left-0 h-full w-1/2 object-cover animate-bounce" src="img/l_curtains.png" alt="левая штора">
-            <img class=" curtain2 absolute top-0 right-0 h-full w-1/2 object-cover animate-bounce" src="img/r_curtains.png" alt="правая штора ">
+            <div class="curtains">
+                <div class="curtains">
+                    <img class="curtain1 absolute top-0 left-0 h-full w-1/2 object-cover " src="img/l_curtains.png"
+                        alt="левая штора">
+                    <img class="curtain2 absolute top-0 right-0 h-full w-1/2 object-cover " src="img/r_curtains.png"
+                        alt="правая штора ">
+                </div>
+
+            </div>
+
         </div>
     </div>
 
