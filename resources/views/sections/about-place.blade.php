29329ab232ea1d7edbf371341e1f8c96b14ac4b0
leritt1 <Lizzka.g@bk.ru>
2026-06-30 09:37:35 +0500

    правки

--- resources/views/sections/about-place.blade.php
+++ resources/views/sections/about-place.blade.php
@@ -1,36 +1,34 @@
-
 <div class="bg-red w-screen p-[120px] md:p-[60px] sm:p-[20px]">
     <h1 class="font-title text-[64px] uppercase text-white mb-[70px]">
-<div class="w-screen bg-red p-[20px] md:p-[60px] xl:p-[120px]">
-    <h1 class="font-title leading-[120%] text-[36px] md:text-[64px] uppercase text-white mb-[50px] md:mb-[70px] w-fit">
-        О ПРОСТРАНСТВЕ «БЮРОКРАТЪ»
-    </h1>
-    <div class="flex flex-col-reverse xl:flex-row gap-y-[20px] xl:gap-y-0 xl:gap-x-[20px]  mb-[50px] xl:mb-[70px]">
-        <div class="flex-1">
-            <p class="font-main text-[16px] xl:text-[20px] leading-[120%] mb-[32px] text-white">
-                Здесь история встречается с современной городской жизнью. Здесь
-                можно рассматривать экспонаты, выпить кофе, полистать книги,
-                пообщаться с друзьями и найти необычные сувениры.
-            </p>
-            <h3
-                class="font-title text-[24px] md:text-[36px] xl:text-[44px] uppercase leading-[120%] text-white"
-            >
-                Мы — живое, интерактивное пространство в историческом центре
-                города Миасс!
-            </h3>
+        <div class="w-screen bg-red p-[20px] md:p-[60px] xl:p-[120px]">
+            <h1 class="font-title leading-[120%] text-[36px] md:text-[64px] uppercase text-white mb-[50px] md:mb-[70px] w-fit">
+                О ПРОСТРАНСТВЕ «БЮРОКРАТЪ»
+            </h1>
+            <div class="flex flex-col-reverse xl:flex-row gap-y-[20px] xl:gap-y-0 xl:gap-x-[20px]  mb-[50px] xl:mb-[70px]">
+                <div class="flex-1">
+                    <p class="font-main text-[16px] xl:text-[20px] leading-[120%] mb-[32px] text-white">
+                        Здесь история встречается с современной городской жизнью. Здесь
+                        можно рассматривать экспонаты, выпить кофе, полистать книги,
+                        пообщаться с друзьями и найти необычные сувениры.
+                    </p>
+                    <h3
+                        class="font-title text-[24px] md:text-[36px] xl:text-[44px] uppercase leading-[120%] text-white">
+                        Мы — живое, интерактивное пространство в историческом центре
+                        города Миасс!
+                    </h3>
+                </div>
+                <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-1.png" alt="">
+            </div>
+            <div class="flex flex-col xl:flex-row gap-y-[20px] xl:gap-y-0 xl:gap-x-[20px] mb-[50px] xl:mb-[70px]">
+                <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-2.png" alt="">
+                <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-3.png" alt="">
+                <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-4.png" alt="">
+            </div>
+            <div class="flex flex-col-reverse xl:flex-row gap-y-[20px] xl:gap-y-0 xl:gap-x-[20px] mb-[50px] xl:mb-[70px]">
+                <img src="img/img_about-5.png" alt="" class="flex-1 w-full h-auto xl:w-fit xl:h-fit">
+                <div class="flex-1">
+                    <h3 class="font-title text-[24px] md:text-[36px] xl:text-[44px] uppercase leading-[120%] mb-[32px] text-white">Почему «бюрократЪ»?</h3>
+                    <p class="font-main text-[16px] xl:text-[20px] leading-[120%] text-white">Название отсылает к атмосфере старых бюро, архивов и учреждений прошлого века. Мы сохранили этот характер в интерьере, наполнив пространство предметами, которые помогают по‑новому взглянуть на историю и провести время в особой ретро-атмосфере.</p>
+                </div>
+            </div>
         </div>
-        <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-1.png" alt="">
-    </div>
-    <div class="flex flex-col xl:flex-row gap-y-[20px] xl:gap-y-0 xl:gap-x-[20px] mb-[50px] xl:mb-[70px]">
-        <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-2.png" alt="">
-        <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-3.png" alt="">
-        <img class="flex-1 w-full h-auto xl:w-fit xl:h-fit" src="img/img_about-4.png" alt="">
-    </div>
-    <div class="flex flex-col-reverse xl:flex-row gap-y-[20px] xl:gap-y-0 xl:gap-x-[20px] mb-[50px] xl:mb-[70px]">
-        <img src="img/img_about-5.png" alt="" class="flex-1 w-full h-auto xl:w-fit xl:h-fit">
-        <div class="flex-1">
-            <h3 class="font-title text-[24px] md:text-[36px] xl:text-[44px] uppercase leading-[120%] mb-[32px] text-white">Почему «бюрократЪ»?</h3>
-            <p class="font-main text-[16px] xl:text-[20px] leading-[120%] text-white">Название отсылает к атмосфере старых бюро, архивов и учреждений прошлого века. Мы сохранили этот характер в интерьере, наполнив пространство предметами, которые помогают по‑новому взглянуть на историю и провести время в особой ретро-атмосфере.</p>
-        </div>
-    </div>
-</div>
