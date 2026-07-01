1cbc9e9795eff96dc8cba5049c67115813dd8b1a
Arina2810 <abuivan@inbox.ru>
2026-07-01 11:44:01 +0500

    news

--- resources/views/sections/news.blade.php
+++ resources/views/sections/news.blade.php
@@ -1,99 +1,152 @@
-<section class="events">
-    <div class="container">
+<section class="py-[60px]">
+    <div class="max-w-[1440px] mx-auto px-5">
         <div class="relative mb-8 lg:mb-10">
             <div
-                class="absolute top-0 left-0 text-[70px] md:text-[100px] xl:text-[130px] font-bold uppercase text-red font-main blur-[6px] opacity-30 pointer-events-none leading-none"
-            >
+                class="absolute top-0 left-0 text-[70px] md:text-[100px] xl:text-[130px] font-bold uppercase text-[#83372A] font-['EB_Garamond'] blur-[6px] opacity-30 pointer-events-none leading-none">
                 бюрократъ
             </div>
         </div>
-        <h1 class="title1">СОБЫТИЯ И НОВОСТИ</h1>
-
-        <div class="events_nav">
-            <button class="btn-nav prev">&#8592;</button>
-            <button class="btn-nav next">&#8594;</button>
+        <h1
+            class="mb-[25px] text-[64px] font-bold text-[#83372A] font-['EB_Garamond']">
+            СОБЫТИЯ И НОВОСТИ
+        </h1>
+        <div class="flex gap-[10px] mb-[30px]">
+            <button
+                class="prev w-[60px] h-[30px] rounded-full bg-[#83372A] text-white text-[25px] flex items-center justify-center leading-none cursor-pointer">
+                &#8592;
+            </button>
+            <button
+                class="next w-[60px] h-[30px] rounded-full bg-[#83372A] text-white text-[25px] flex items-center justify-center leading-none cursor-pointer">
+                &#8594;
+            </button>
         </div>
-        
-      
-                <div class="embla" id="my-carousel">
-                    <div class="embla__viewport">
-                        <div class="embla__container">
-                            <div class="embla__slide">
-                                <article class="event-card">
-                                    <img src="img/img1.png" alt="" />
-                                    <div class="card_content">
-                                        <h3 class="font-medium">
-                                            Фестиваль «Между домами»
-                                        </h3>
-                                        <p>21 июня с 13:00–20:00</p>
-                                    </div>
-                                </article>
+        <div class="embla" id="my-carousel">
+            <div class="embla__viewport">
+                <div class="embla__container">
+                    <div class="embla__slide">
+                        <article
+                            class="flex flex-col items-center w-[438px] h-[596px] border border-[#83372A] mb-[56px]">
+                            <img
+                                src="img/img1.png"
+                                alt=""
+                                class="w-[398px] h-[471px] pt-5 object-cover"/>
+                            <div>
+                                <h3
+                                    class="pt-[29px] text-[24px] text-center text-[#3F3F3F] font-medium">
+                                    Фестиваль «Между домами»
+                                </h3>
+                                <p
+                                    class="text-[24px] text-center text-[#3F3F3F]">
+                                    21 июня с 13:00–20:00
+                                </p>
                             </div>
-                            <div class="embla__slide">
-                                <article class="event-card">
-                                    <img src="img/img2.png" alt="" />
-                                    <div class="card_content">
-                                        <h3 class="font-medium">
-                                            Ретрозабег по Старгороду
-                                        </h3>
-                                        <p>
-                                            14 июня, 5 июля, 15 августа в 10:00
-                                        </p>
-                                    </div>
-                                </article>
-                            </div>
-                            <div class="embla__slide">
-                                <article class="event-card">
-                                    <img src="img/img3.png" alt="" />
-                                    <div class="card_content">
-                                        <h3 class="font-medium">
-                                            Регистрация на забег
-                                        </h3>
-                                        <p>14 июня</p>
-                                    </div>
-                                </article>
-                            </div>
-                            
-                            <div class="embla__slide">
-                                <article class="event-card">
-                                    <img src="img/img3.png" alt="" />
-                                    <div class="card_content">
-                                        <h3 class="font-medium">
-                                            Регистрация на забег
-                                        </h3>
-                                        <p>14 июня</p>
-                                    </div>
-                                </article>
-                            </div>
-                            <div class="embla__slide">
-                                <article class="event-card">
-                                    <img src="img/img3.png" alt="" />
-                                    <div class="card_content">
-                                        <h3 class="font-medium">
-                                            Регистрация на забег
-                                        </h3>
-                                        <p>14 июня</p>
-                                    </div>
-                                </article>
-                            </div>
-                            <div class="embla__slide">
-                                <article class="event-card">
-                                    <img src="img/img3.png" alt="" />
-                                    <div class="card_content">
-                                        <h3 class="font-medium">
-                                            Регистрация на забег
-                                        </h3>
-                                        <p>14 июня</p>
-                                    </div>
-                                </article>
-                            </div>
-                        </div>
+                        </article>
                     </div>
-                    <div class="events_footer">
-                        <a href="#" class="btn_brn">Забронировать</a>
+                    <div class="embla__slide">
+                        <article
+                            class="flex flex-col items-center w-[438px] h-[596px] border border-[#83372A] mb-[56px]">
+                            <img
+                                src="img/img2.png"
+                                alt=""
+                                class="w-[398px] h-[471px] pt-5 object-cover"/>
+                            <div>
+                                <h3
+                                    class="pt-[29px] text-[24px] text-center text-[#3F3F3F] font-medium">
+                                    Ретрозабег по Старгороду
+                                </h3>
+                                <p
+                                    class="text-[24px] text-center text-[#3F3F3F]">
+                                    14 июня, 5 июля, 15 августа в 10:00
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="embla__slide">
+                        <article
+                            class="flex flex-col items-center w-[438px] h-[596px] border border-[#83372A] mb-[56px]">
+                            <img
+                                src="img/img3.png"
+                                alt=""
+                                class="w-[398px] h-[471px] pt-5 object-cover"/>
+
+                            <div>
+                                <h3
+                                    class="pt-[29px] text-[24px] text-center text-[#3F3F3F] font-medium">
+                                    Регистрация на забег
+                                </h3>
+                                <p
+                                    class="text-[24px] text-center text-[#3F3F3F]">
+                                    14 июня
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="embla__slide">
+                        <article
+                            class="flex flex-col items-center w-[438px] h-[596px] border border-[#83372A] mb-[56px]">
+                            <img
+                                src="img/img3.png"
+                                alt=""
+                                class="w-[398px] h-[471px] pt-5 object-cover"/>
+                            <div>
+                                <h3
+                                    class="pt-[29px] text-[24px] text-center text-[#3F3F3F] font-medium">
+                                    Регистрация на забег
+                                </h3>
+                                <p
+                                    class="text-[24px] text-center text-[#3F3F3F]">
+                                    14 июня
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="embla__slide">
+                        <article
+                            class="flex flex-col items-center w-[438px] h-[596px] border border-[#83372A] mb-[56px]">
+                            <img
+                                src="img/img3.png"
+                                alt=""
+                                class="w-[398px] h-[471px] pt-5 object-cover"/>
+                            <div>
+                                <h3
+                                    class="pt-[29px] text-[24px] text-center text-[#3F3F3F] font-medium">
+                                    Регистрация на забег
+                                </h3>
+                                <p
+                                    class="text-[24px] text-center text-[#3F3F3F]">
+                                    14 июня
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="embla__slide">
+                        <article
+                            class="flex flex-col items-center w-[438px] h-[596px] border border-[#83372A] mb-[56px]">
+                            <img
+                                src="img/img3.png"
+                                alt=""
+                                class="w-[398px] h-[471px] pt-5 object-cover"/>
+                            <div>
+                                <h3
+                                    class="pt-[29px] text-[24px] text-center text-[#3F3F3F] font-medium">
+                                    Регистрация на забег
+                                </h3>
+                                <p
+                                    class="text-[24px] text-center text-[#3F3F3F]">
+                                    14 июня
+                                </p>
+                            </div>
+                        </article>
                     </div>
                 </div>
-       
             </div>
-    
+            <div class="mt-6 flex justify-start">
+                <a
+                    href="#"
+                    class="px-10 py-[14px] rounded-full bg-[#83372A] text-[#E7E5E0] text-[20px] transition duration-300 hover:opacity-90">
+                    Забронировать
+                </a>
+            </div>
+        </div>
+    </div>
 </section>
